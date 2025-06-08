0e649d3a0a531c0f2fcd152df90c14ee0ff85ed1
Iron Levyy <olegan.plav@gmail.com>
2025-06-08 17:09:06 +0300

    initial commit

---
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -0,0 +1,61 @@
+package pro.sky.telegrambot.model;
+
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notification_task")
+public class NotificationTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "chat_id", nullable = false)
+    private Long chatId;
+
+    @Column(name = "notification_text", nullable = false)
+    private String notificationText;
+
+    @Column(name = "notification_datetime", nullable = false)
+    private LocalDateTime notificationDateTime;
+
+    public NotificationTask() {}
+
+    public NotificationTask(Long chatId, String notificationText, LocalDateTime notificationDateTime) {
+        this.chatId = chatId;
+        this.notificationText = notificationText;
+        this.notificationDateTime = notificationDateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getNotificationText() {
+        return notificationText;
+    }
+
+    public void setNotificationText(String notificationText) {
+        this.notificationText = notificationText;
+    }
+
+    public LocalDateTime getNotificationDateTime() {
+        return notificationDateTime;
+    }
+
+    public void setNotificationDateTime(LocalDateTime notificationDateTime) {
+        this.notificationDateTime = notificationDateTime;
+    }
+}
+
